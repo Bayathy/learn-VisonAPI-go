b46f3450e52f5155c3fe87cebbe2133bb33699f2
Bayathy <misosoup0321@icloud.com>
2022-05-11 09:08:54 +0900

    init commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+        "context"
+        "fmt"
+        "log"
+        "os"
+
+        vision "cloud.google.com/go/vision/apiv1"
+)
+
+func main() {
+        ctx := context.Background()
+
+        // Creates a client.
+        client, err := vision.NewImageAnnotatorClient(ctx)
+        if err != nil {
+                log.Fatalf("Failed to create client: %v", err)
+        }
+        defer client.Close()
+
+        // Sets the name of the image file to annotate.
+        filename := "../testdata/cat.jpg"
+
+        file, err := os.Open(filename)
+        if err != nil {
+                log.Fatalf("Failed to read file: %v", err)
+        }
+        defer file.Close()
+        image, err := vision.NewImageFromReader(file)
+        if err != nil {
+                log.Fatalf("Failed to create image: %v", err)
+        }
+
+        labels, err := client.DetectLabels(ctx, image, nil, 10)
+        if err != nil {
+                log.Fatalf("Failed to detect labels: %v", err)
+        }
+
+        fmt.Println("Labels:")
+        for _, label := range labels {
+                fmt.Println(label.Description)
+        }
+}
